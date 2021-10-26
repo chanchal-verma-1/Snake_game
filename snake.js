a2828922e374f67b1f42820259d7d637dca23fe3
stuti agarwal <stutiagarwal0909@gmail.com>
2021-10-26 13:33:59 +0530

    Added snake and key functionalities

--- snake.js
+++ snake.js
@@ -1,2 +1,23 @@
+let snake = [];
 
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+};
+let d;
+
+document.addEventListener("keydown",direction);
+
+function direction(event){
+    let key = event.keyCode;
+    if( key == 37 && d!="RIGHT" ){
+        d = "LEFT";
+    }else if(key == 38 && d!="DOWN" ){
+        d = "UP";
+    }else if(key == 39 && d!="LEFT"){
+        d = "RIGHT";
+    }else if(key == 40 && d!="UP"){
+        d = "DOWN";
+    }
+}
 

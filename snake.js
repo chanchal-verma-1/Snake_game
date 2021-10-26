ddac8efc870cacd314ec4008c43d05405007fc98
stuti agarwal <stutiagarwal0909@gmail.com>
2021-10-26 20:15:27 +0530

    Added food and score

--- snake.js
+++ snake.js
@@ -18,12 +18,9 @@ function direction(event){
         d = "DOWN";
     }
 }
-
 let food = {
     x : Math.floor(Math.random()*17+1) * box,
     y : Math.floor(Math.random()*15+3) * box
 }
-
 let score = 0;
-
 

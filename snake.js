dbe95ec0cf55c4694c5015bf6a61d81c5d041c49
Chanchal Verma <92948885+chanchal-verma-1@users.noreply.github.com>
2021-10-26 22:16:25 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,3 +1,21 @@
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// load audio files
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
 let snake = [];
 
 snake[0] = {

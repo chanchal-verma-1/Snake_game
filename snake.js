d355c33b3a797b285460e672c1adda90ef96c61d
Chanchal Verma <92948885+chanchal-verma-1@users.noreply.github.com>
2021-10-26 20:41:29 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -25,34 +25,6 @@ let food = {
 }
 let score = 0;
 
-const cvs = document.getElementById("snake");
-const ctx = cvs.getcontext("2d");
-
-const box = 32;
-
- const ground = new Image();
- ground.src = "img/ground.png";
-
-const foodImg = new Image();
-foodImg.src = "img/food.png";
-
-// load audio files
-
-let dead = new Audio();
-let eat = new Audio();
-let up = new Audio();
-let right = new Audio();
-let left = new Audio();
-let down = new Audio();
-
-dead.src = "audio/dead.mp3";
-eat.src = "audio/eat.mp3";
-up.src = "audio/up.mp3";
-right.src = "audio/right.mp3";
-left.src = "audio/left.mp3";
-down.src = "audio/down.mp3";
-
-
 function collision(head,array){
     for(let i = 0; i < array.length; i++){
         if(head.x == array[i].x && head.y == array[i].y){
